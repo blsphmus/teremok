12d4e782aaa487ed9723da210d3090dcc3db27e5
Roguelied <alirurpha@gmail.com>
2024-11-17 01:47:34 +1000

    Init

---
+++ Source/towerdef/Private/StaticCameraGameMode.cpp
@@ -0,0 +1,34 @@
+#include "StaticCameraGameMode.h"
+#include "Camera/CameraActor.h"
+#include "Camera/CameraComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+void AStaticCameraGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Find the first CameraActor in the level
+	TArray<AActor*> FoundActors;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), ACameraActor::StaticClass(), FoundActors);
+	
+	
+	if (FoundActors.Num() > 0)
+	{
+		// Assume the first CameraActor is the one we want to use
+		ACameraActor* DefaultCamera = Cast<ACameraActor>(FoundActors[0]);
+		UE_LOG(LogTemp, Warning, TEXT("Camera Component Forward Vector: %s"), *DefaultCamera->GetCameraComponent()->GetForwardVector().ToString());
+
+
+		if (DefaultCamera)
+		{
+			// Get the player controller
+			APlayerController* PlayerController = UGameplayStatics::GetPlayerController(this, 0);
+
+			if (PlayerController)
+			{
+				// Set the view target to the default camera
+				PlayerController->SetViewTarget(DefaultCamera);
+			}
+		}
+	}
+}
